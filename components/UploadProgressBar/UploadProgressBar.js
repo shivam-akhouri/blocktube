082901295b826c4bc054bad3fdb8ac7086060d5d
Suraj Shah <surajshah.bombay@gmail.com>
2022-07-31 08:50:56 +0530

    Made progressBar properly responsive

--- components/UploadProgressBar/UploadProgressBar.js
+++ components/UploadProgressBar/UploadProgressBar.js
@@ -6,27 +6,25 @@ const UploadProgressBar = () => {
     <>
     <div className={styles.bar}>
 
-      <div className={styles.barText}>
-        <span >Upload Video</span>
-        <span className={styles.activeText}>Details</span>
-        <span>Thumbnail</span>  
-        <span>Preview</span>
-      </div>
     <div className={styles.progressBar}>
       <div className={styles.progressElements}>
+        <p className={styles.head1}>Upload Video</p>
         <div className={[styles.circle, styles.completed].join(" ")}></div>
         </div>
 
         <div className={styles.progressElements}>
-        <div className={[styles.circle, styles.active].join(" ")}></div>
+          <p className={[styles.activeText, styles.head2].join(" ")}>Details</p>
+          <div className={[styles.circle, styles.active].join(" ")}></div>
         </div>
         
         <div className={styles.progressElements}>
-        <div className={styles.circle}></div>
+          <p className={styles.head3}>Thumbnail</p>  
+          <div className={styles.circle}></div>
         </div>
 
         <div className={styles.progressElements}>
-        <div className={styles.circle}></div>
+          <p className={styles.head4}>Preview</p>
+          <div className={styles.circle}></div>
         </div>
         
     </div>
